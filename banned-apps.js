288cac9138674d0a83fd900ca33dfd18ece5e5d2
OddOmens <74390402+OddOmens@users.noreply.github.com>
2025-11-20 09:50:47 -0600

    Add banned apps configuration file

---
+++ banned-apps.js
@@ -0,0 +1,126 @@
+{
+    "version": "1.0.0",
+    "lastUpdated": "2025-11-20T15:49:00Z",
+    "bannedApps": [
+        {
+            "appId": "284882215",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Facebook"
+        },
+        {
+            "appId": "389801252",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Instagram"
+        },
+        {
+            "appId": "454638411",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Messenger"
+        },
+        {
+            "appId": "310633997",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for WhatsApp"
+        },
+        {
+            "appId": "333903271",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Twitter/X"
+        },
+        {
+            "appId": "835599320",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for TikTok"
+        },
+        {
+            "appId": "544007664",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for YouTube"
+        },
+        {
+            "appId": "447188370",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Snapchat"
+        },
+        {
+            "appId": "6448311069",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for ChatGPT"
+        },
+        {
+            "appId": "288429040",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for LinkedIn"
+        },
+        {
+            "appId": "422689480",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Gmail"
+        },
+        {
+            "appId": "585027354",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Google Maps"
+        },
+        {
+            "appId": "284815942",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Google"
+        },
+        {
+            "appId": "1482454543",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Google Chrome"
+        },
+        {
+            "appId": "1059655371",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Microsoft Edge"
+        },
+        {
+            "appId": "1018301773",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Amazon"
+        },
+        {
+            "appId": "376510438",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Spotify"
+        },
+        {
+            "appId": "1477376905",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Zoom"
+        },
+        {
+            "appId": "1176074088",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Slack"
+        },
+        {
+            "appId": "1113153706",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Microsoft Teams"
+        },
+        {
+            "appId": "1217041820",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Telegram"
+        },
+        {
+            "appId": "539883307",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Waze"
+        },
+        {
+            "appId": "1569813810",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Perplexity"
+        },
+        {
+            "appId": "1438147202",
+            "banType": "marketingOnly",
+            "message": "Marketing disabled for Claude"
+        }
+    ]
+}
